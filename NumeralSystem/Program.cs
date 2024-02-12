3e5e6d18fbb5c0e5b463a5c610b924b4c353b57a
PC-23 <it.pgee.burgas@gmail.com>
2024-02-12 12:56:41 +0200

    .

---
+++ NumeralSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NumeralSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter system: ");
+            int numberSystem = int.Parse(Console.ReadLine());
+            var answer = Convert.ToString(num, numberSystem);
+            Console.WriteLine(answer);
+        }
+    }
+}
